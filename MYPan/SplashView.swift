c8db5f7c67dc073394708dd4e84f1116c7c0ceb8
comet959 <1376723538@qq.com>
2024-01-04 05:21:31 +0800

    fix: add some change.

--- MYPan/SplashView.swift
+++ MYPan/SplashView.swift
@@ -5,4 +5,35 @@
 //  Created by 杨竣轶 on 2024/1/4.
 //
 
-import Foundation
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive: Bool = false
+
+    var body: some View {
+        VStack {
+            if isActive {
+                OnboardingView()
+            } else {
+                Text("App Logo")
+                    .font(.largeTitle)
+                Text("欢迎使用HPan")
+                    .font(.headline)
+            }
+        }
+        .onAppear {
+            // 延迟切换到引导页
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
