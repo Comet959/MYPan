fd29d1576edcf5fea18931658c4f8a2ef5233356
comet959 <1376723538@qq.com>
2024-01-04 04:33:11 +0800

    Initial Commit

---
+++ MYPan/MYPanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYPanApp.swift
+//  MYPan
+//
+//  Created by 杨竣轶 on 2024/1/4.
+//
+
+import SwiftUI
+
+@main
+struct MYPanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
